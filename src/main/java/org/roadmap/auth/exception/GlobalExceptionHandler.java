af477f2115960b5dabeaeb55c56bdc6345e7e8bb
Kamil1developer <igologramma@mail.ru>
2026-08-26 15:08:02 +0300

    добавил BadCredentialsException

--- src/main/java/org/roadmap/auth/exception/GlobalExceptionHandler.java
+++ src/main/java/org/roadmap/auth/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package org.roadmap.auth.exception;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,10 +25,19 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.CONFLICT)
                 .body(new ErrorResponse("409", "пользователь занят"));
     }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleUnexpectedException(){
         return ResponseEntity
                 .badRequest()
                 .body(new ErrorResponse("500", "неизвестная ошибка"));
     }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleBadCredentials(){
+        return ResponseEntity.
+                status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse("401","неверные данные (такого пользователя нет, или пароль неправильный)"));
+    }
+
 }
