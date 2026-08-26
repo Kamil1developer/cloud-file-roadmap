4c1d2abb79fd160c2f3a98ebf7b25d46362ccee2
Kamil1developer <igologramma@mail.ru>
2026-08-26 09:39:42 +0300

    сделал json контракт ошибки ErrorResponse, а также ошибки 400 409 500

---
+++ src/main/java/org/roadmap/auth/exception/ErrorResponse.java
@@ -0,0 +1,5 @@
+package org.roadmap.auth.exception;
+
+public record ErrorResponse(String code, String message) {
+
+}
--- src/main/java/org/roadmap/auth/exception/GlobalExceptionHandler.java
+++ src/main/java/org/roadmap/auth/exception/GlobalExceptionHandler.java
@@ -1,4 +1,33 @@
 package org.roadmap.auth.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(){
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse("400", "ошибка валидации"));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateException(){
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorResponse("409", "пользователь занят"));
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnexpectedException(){
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse("500", "неизвестная ошибка"));
+    }
 }
