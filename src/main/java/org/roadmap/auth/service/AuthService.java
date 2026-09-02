afb56e9e8391856f1a58cb95779cc579642065f1
Kamil1developer <igologramma@mail.ru>
2026-09-02 16:49:31 +0300

    убрал не нужные библиотеки

--- src/main/java/org/roadmap/auth/service/AuthService.java
+++ src/main/java/org/roadmap/auth/service/AuthService.java
@@ -1,18 +1,13 @@
 package org.roadmap.auth.service;
 
 import lombok.RequiredArgsConstructor;
-import org.roadmap.auth.dto.LoginUserDto;
 import org.roadmap.auth.dto.RegisterUserDto;
 import org.roadmap.auth.dto.response.UserResponse;
 import org.roadmap.auth.mapper.UserMapper;
 import org.roadmap.entity.User;
 import org.roadmap.repository.UserRepository;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
