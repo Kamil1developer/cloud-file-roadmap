2a5bdf1d0ef6ccef81e9c39132c92fdeac2ca7e9
Kamil1developer <igologramma@mail.ru>
2026-09-08 09:57:04 +0300

    онтроллер пользователя перенесён в общий пакет контроллеров. Для ответа текущего пользователя указан точный тип ResponseEntity<Map<String, String>>.

--- src/main/java/org/roadmap/controller/UserController.java
+++ src/main/java/org/roadmap/controller/UserController.java
@@ -1,15 +1,14 @@
-package org.roadmap.auth.controller;
+package org.roadmap.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -17,7 +16,7 @@ import java.util.Map;
 public class UserController {
     @Operation(summary = "Текущий пользователь")
     @GetMapping("me")
-    public ResponseEntity getCurrentUser(Authentication authentication){
+    public ResponseEntity<Map<String, String>> getCurrentUser(Authentication authentication){
 
         return ResponseEntity.
                 status(HttpStatus.OK)
