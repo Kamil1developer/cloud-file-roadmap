d1ff3e1af6a6a47e931e0c63a86c2906b110fd3e
Kamil1developer <igologramma@mail.ru>
2026-09-08 09:59:16 +0300

    получение содержимого директории переведено на метод getContentByDirectory. Добавлен метод создания директории с преобразованием результата в CreatedDirectoryResponse.

--- src/main/java/org/roadmap/service/DirectoryService.java
+++ src/main/java/org/roadmap/service/DirectoryService.java
@@ -1,9 +1,11 @@
 package org.roadmap.service;
 
 import lombok.RequiredArgsConstructor;
+import org.roadmap.dto.response.CreatedDirectoryResponse;
 import org.roadmap.dto.response.DirectoryContentResponse;
 import org.roadmap.mapper.DirectoryContentMapper;
 import org.roadmap.storage.MinioStorage;
+import org.roadmap.storage.model.DirectoryResource;
 import org.roadmap.storage.model.StorageResource;
 import org.springframework.stereotype.Service;
 
@@ -16,9 +18,13 @@ public class DirectoryService {
     private final DirectoryContentMapper mapper;
 
     public List<DirectoryContentResponse> getContent(String path){
-        List<StorageResource> resource = storage.getContent(path);
+        List<StorageResource> resource = storage.getContentByDirectory(path);
         return mapper.toDirectoryContentResponse(resource);
     }
 
 
+    public CreatedDirectoryResponse createDirectory(String path) {
+        DirectoryResource resource = storage.createDirectoryByPath(path);
+        return mapper.toCreatedDirectoryResponse(resource);
+    }
 }
