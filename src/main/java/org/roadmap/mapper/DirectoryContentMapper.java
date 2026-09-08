b25710f20c9147ee88f3612c1f70474583edb50d
Kamil1developer <igologramma@mail.ru>
2026-09-08 09:58:20 +0300

    добавлено преобразование DirectoryResource в CreatedDirectoryResponse.

--- src/main/java/org/roadmap/mapper/DirectoryContentMapper.java
+++ src/main/java/org/roadmap/mapper/DirectoryContentMapper.java
@@ -1,7 +1,9 @@
 package org.roadmap.mapper;
 
 import org.mapstruct.Mapper;
+import org.roadmap.dto.response.CreatedDirectoryResponse;
 import org.roadmap.dto.response.DirectoryContentResponse;
+import org.roadmap.storage.model.DirectoryResource;
 import org.roadmap.storage.model.StorageResource;
 
 import java.util.List;
@@ -9,4 +11,6 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface DirectoryContentMapper {
     public List<DirectoryContentResponse> toDirectoryContentResponse(List<StorageResource> resource);
+    public CreatedDirectoryResponse toCreatedDirectoryResponse(DirectoryResource resource);
+
 }
