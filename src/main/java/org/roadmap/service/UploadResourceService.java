30ab94dc1a65baffe7202eadf2563955bcb9d717
Kamil1developer <igologramma@mail.ru>
2026-09-08 09:59:45 +0300

    добавлен сервис загрузки одного или нескольких файлов. Он формирует ObjectUploadRequest, загружает файлы в MinIO и возвращает список UploadResponse. При отсутствии файлов возвращается Optional.empty().

---
+++ src/main/java/org/roadmap/service/UploadResourceService.java
@@ -0,0 +1,61 @@
+package org.roadmap.service;
+import lombok.RequiredArgsConstructor;
+import org.roadmap.dto.response.UploadResponse;
+import org.roadmap.mapper.ResourceMapper;
+import org.roadmap.storage.MinioStorage;
+import org.roadmap.storage.dto.request.ObjectUploadRequest;
+import org.roadmap.storage.model.StorageResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UploadResourceService {
+    private final MinioStorage storage;
+    private final ResourceMapper mapper;
+
+    public Optional<List<UploadResponse>> upload(List<MultipartFile> files) throws IOException {
+        if (files.size() == 1) {
+            MultipartFile file = files.getFirst();
+            ObjectUploadRequest uploadRequest = new ObjectUploadRequest(
+                    file.getOriginalFilename(),
+                    file.getInputStream(),
+                    file.getSize(),
+                    file.getContentType()
+            );
+
+            StorageResource storageResource = storage.upload(uploadRequest);
+
+            UploadResponse uploadResponse = mapper.toUploadResponse(storageResource);
+
+
+
+            return Optional.of(List.of(uploadResponse));
+        }
+        if (files.size() > 1) {
+            List<UploadResponse> uploadResponses = new ArrayList<>();
+            for (MultipartFile file : files) {
+
+                ObjectUploadRequest uploadRequest = new ObjectUploadRequest(
+                        file.getOriginalFilename(),
+                        file.getInputStream(),
+                        file.getSize(),
+                        file.getContentType()
+                );
+                String type = file.getContentType();
+                StorageResource storageResources = storage.upload(uploadRequest);
+                UploadResponse uploadResponse = mapper.toUploadResponse(storageResources);
+
+                uploadResponses.add(uploadResponse);
+            }
+            return Optional.of(uploadResponses);
+
+        }
+        return Optional.empty();
+    }
+}
