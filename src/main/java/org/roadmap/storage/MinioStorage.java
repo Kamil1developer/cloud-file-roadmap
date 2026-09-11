3c2d27bb43fa24bb1b80ca7696f611c5c5a27317
Kamil1developer <igologramma@mail.ru>
2026-09-11 13:44:46 +0300

    feat: расширить интерфейс операциями удаления, перемещения, скачивания и поиска

--- src/main/java/org/roadmap/storage/MinioStorage.java
+++ src/main/java/org/roadmap/storage/MinioStorage.java
@@ -6,14 +6,21 @@ import org.roadmap.storage.dto.request.ObjectUploadRequest;
 import org.roadmap.storage.model.DirectoryResource;
 import org.roadmap.storage.model.StorageResource;
 
+import java.io.InputStream;
 import java.util.List;
 
 public interface MinioStorage {
     StorageResource  upload(ObjectUploadRequest uploadRequest);
     List<StorageResource> getContentByDirectory(String path);
     void deleteByPath(String path);
+    void deleteByPrefix(String path);
     StorageResource getResourceByPath(String path);
     DirectoryResource createDirectoryByPath(String path);
+    StorageResource moveOrRenameObject(String from, String to);
+    InputStream downloadResourceByPath(String path);
+    List<StorageResource> findResourcesByName(String query);
+    List<StorageResource> findAllResourcesByPrefix(String prefix);
+
 
 
 }
