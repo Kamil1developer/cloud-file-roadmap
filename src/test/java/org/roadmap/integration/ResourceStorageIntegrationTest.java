77e030656ea6966705fd237413e1573fb4b88801
Kamil1developer <igologramma@mail.ru>
2026-09-11 13:45:48 +0300

    test: добавить основу интеграционного теста хранилища ресурсов

---
+++ src/test/java/org/roadmap/integration/ResourceStorageIntegrationTest.java
@@ -0,0 +1,9 @@
+package org.roadmap.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ResourceStorageIntegrationTest {
+}
