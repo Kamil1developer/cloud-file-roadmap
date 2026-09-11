28bd7349fa86b62241b69770468be9386a44352a
Kamil1developer <igologramma@mail.ru>
2026-09-11 13:45:33 +0300

    refactor: сделать размер ресурса nullable для пустых директорий

--- src/main/java/org/roadmap/storage/model/StorageResource.java
+++ src/main/java/org/roadmap/storage/model/StorageResource.java
@@ -1,9 +1,13 @@
 package org.roadmap.storage.model;
 
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 public record StorageResource(
         String path,
         String name,
-        long size,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        Long size,
         String type
 ){
 }
