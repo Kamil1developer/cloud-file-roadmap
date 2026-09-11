f2196c1e6e7f4a9c87117ccebeb91dd5a2c4932a
Kamil1developer <igologramma@mail.ru>
2026-09-11 13:43:08 +0300

    feat: добавить преобразование списка ресурсов в результаты поиска

--- src/main/java/org/roadmap/mapper/ResourceMapper.java
+++ src/main/java/org/roadmap/mapper/ResourceMapper.java
@@ -1,11 +1,17 @@
 package org.roadmap.mapper;
 
 import org.mapstruct.Mapper;
+import org.roadmap.dto.response.MoveOrRenameResponse;
+import org.roadmap.dto.response.SearchResourceResponse;
 import org.roadmap.dto.response.UploadResponse;
 import org.roadmap.storage.model.StorageResource;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ResourceMapper {
     public UploadResponse toUploadResponse(StorageResource storageResource);
+    public List<SearchResourceResponse> toSearchResponse(List<StorageResource> resources);
+
 
 }
