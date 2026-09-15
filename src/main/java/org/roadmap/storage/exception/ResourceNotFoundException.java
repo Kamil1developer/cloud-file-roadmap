df9b8c771c7fdaa128b3f5a209aa4af6079984eb
Kamil1developer <igologramma@mail.ru>
2026-09-15 14:11:24 +0300

    public class ResourceNotFoundException extends RuntimeException {

--- src/main/java/org/roadmap/storage/exception/ResourceNotFoundException.java
+++ src/main/java/org/roadmap/storage/exception/ResourceNotFoundException.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 
 @Getter
 public class ResourceNotFoundException extends RuntimeException {
-    private static final String message = "Ресурс не найден";
-
     public ResourceNotFoundException() {
         super("Ресурс не найден");
     }
